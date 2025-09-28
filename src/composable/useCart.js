d44ef6d5a9f68ab664537f168d18619232136ede
Anemoiaa <georgiypetroff@gmail.com>
2025-09-28 21:23:03 +0400

    fix: unexpected cart fetching on visit product page

--- src/composable/useCart.js
+++ src/composable/useCart.js
@@ -3,6 +3,7 @@ import { ref, computed, onMounted } from 'vue'
 import CartApi from '@/services/api/cart.js'
 
 import { DELIVERY_PRICE } from '@/config/app.js'
+import { useAuth } from '@/composable/useAuth.js'
 
 const isOpen = ref(false)
 const cartItems = ref([])
@@ -11,80 +12,85 @@ const cartLength = computed(() => cartItems.value.length)
 const subTotal = computed(() => cartItems.value.reduce((acc, item) => acc + item.total_price, 0))
 const total = computed(() => subTotal.value + DELIVERY_PRICE)
 
-function findCartItem(productId, color, size) {
-  return cartItems.value.find(
-    (item) => item.id === productId && item.color === color && item.size === size,
-  )
-}
-
-function openCart() {
-  isOpen.value = true
-}
-
-function closeCart() {
-  isOpen.value = false
-}
-
-async function fetchCart() {
-  try {
-    cartItems.value = await CartApi.getCart()
-  } catch (error) {
-    toast(error.message, { type: 'error' })
-  }
-}
-
-async function addItem({ productId, color = null, size = null, quantity = 1 }) {
-  const existing = findCartItem(productId, color, size)
-
-  try {
-    if (existing) {
-      await updateItem({ productId, color, size, quantity: existing.quantity + quantity })
-    } else {
-      cartItems.value.push(await CartApi.addItem({ productId, color, size, quantity }))
-    }
-  } catch (error) {
-    toast(error.message, { type: 'error' })
-  }
-}
-
-async function updateItem({ productId, color, size, quantity = 1 }) {
-  const existing = findCartItem(productId, color, size)
-
-  try {
-    await CartApi.updateItem({ productId, quantity })
-
-    existing.quantity = quantity
-    existing.total_price = existing.price * existing.quantity
-    existing.color = color
-    existing.size = size
-  } catch (error) {
-    toast(error.message, { type: 'error' })
-  }
-}
-
-async function removeItem(id) {
-  try {
-    await CartApi.removeItem(id)
-  } catch (error) {
-    toast(error.message, { type: 'error' })
-  }
-
-  await fetchCart()
-}
-
-async function checkout(name, surname, email, address, zipCode) {
-  try {
-    await CartApi.checkout(name, surname, email, address, zipCode)
-
-    return true
-  } catch (error) {
-    toast(error.message, { type: 'error' })
-
-    return false
-  }
-}
-
 export function useCart() {
+
+  function findCartItem(productId, color, size) {
+    return cartItems.value.find(
+      (item) => item.id === productId && item.color === color && item.size === size,
+    )
+  }
+
+  function openCart() {
+    isOpen.value = true
+  }
+
+  function closeCart() {
+    isOpen.value = false
+  }
+
+  async function fetchCart() {
+    if (!useAuth().isAuthenticated()) {
+      return
+    }
+
+    try {
+      cartItems.value = await CartApi.getCart()
+    } catch (error) {
+      toast(error.message, { type: 'error' })
+    }
+  }
+
+  async function addItem({ productId, color = null, size = null, quantity = 1 }) {
+    const existing = findCartItem(productId, color, size)
+
+    try {
+      if (existing) {
+        await updateItem({ productId, color, size, quantity: existing.quantity + quantity })
+      } else {
+        cartItems.value.push(await CartApi.addItem({ productId, color, size, quantity }))
+      }
+    } catch (error) {
+      toast(error.message, { type: 'error' })
+    }
+  }
+
+  async function updateItem({ productId, color, size, quantity = 1 }) {
+    const existing = findCartItem(productId, color, size)
+
+    try {
+      await CartApi.updateItem({ productId, quantity })
+
+      existing.quantity = quantity
+      existing.total_price = existing.price * existing.quantity
+      existing.color = color
+      existing.size = size
+    } catch (error) {
+      toast(error.message, { type: 'error' })
+    }
+  }
+
+  async function removeItem(id) {
+    try {
+      await CartApi.removeItem(id)
+    } catch (error) {
+      toast(error.message, { type: 'error' })
+    }
+
+    await fetchCart()
+  }
+
+  async function checkout(name, surname, email, address, zipCode) {
+    try {
+      await CartApi.checkout(name, surname, email, address, zipCode)
+
+      return true
+    } catch (error) {
+      toast(error.message, { type: 'error' })
+
+      return false
+    }
+  }
+
   onMounted(async () => {
     if (!cartItems.value.length) {
       await fetchCart()
